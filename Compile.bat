437186dc7ac8d094c1451284a3f251b0081d8bd4
Tom <Tom@TOMS-DESKTOP>
2017-07-28 16:28:02 -0400

    Updated the compile batch file.

--- Compile.bat
+++ Compile.bat
@@ -1,3 +1,5 @@
 MSBuild.exe "Restore Point Creator Exported Log Viewer.sln" /noconsolelogger /t:Rebuild /p:Configuration=Debug
 MSBuild.exe "Restore Point Creator Exported Log Viewer.sln" /noconsolelogger /t:Rebuild /p:Configuration=Release
-cscript "C:\Users\Tom\OneDrive\My Visual Studio Projects\MessageBox.vbs" message "Compile complete." "Compile Progress"
+cscript "%OneDrive%\My Visual Studio Projects\MessageBox.vbs" message "Compile complete." "Compile Progress"
+cd "Restore Point Creator Exported Log Viewer\bin\Debug"
+call "copy files.bat"
